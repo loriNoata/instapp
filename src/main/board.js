8f241125eb6d7ecd46f6c25b88b9091f1a150647
Lori Noata <lori.noata@yahoo.com>
2020-03-01 23:25:52 +0200

    main structure with api imgs

---
+++ src/main/board.js
@@ -0,0 +1,43 @@
+import React, {Component, useEffect, useState} from 'react'; 
+ 
+
+ 
+	export default function Board()  {
+    const urlPhotos = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&safe_search=1&format=json&nojsoncallback=1&api_key=15b67c2a8b4288ff1fddf5eb56655cfb&content_type=1&is_getty=1';
+    const [photos, setPhotos] = useState([]);
+    useEffect (() => {
+        fetch(urlPhotos).then(response => {
+            return response.json()
+          }).then( data => {
+            console.log(data);
+            setPhotos(data.photos.photo)
+
+        })
+    })
+
+    const setUrlFromData = (data) => {
+        const {id, farm, server, secret } = data;
+        const photoUrl = `https://farm${farm}.staticflickr.com/${server}/${id}_${secret}.jpg`
+        
+        return photoUrl
+    }
+
+    return (
+        <div className="board-container">  
+            <ul>
+                { photos.map(photo =>(
+                <li className="image-container">
+                    <img  src={setUrlFromData(photo)} key={photo.id} className="image-full"/>  
+                </li>
+                ))}
+            </ul>
+        </div>
+
+		)
+
+    }
+ 
+		
+ 
+ 
+
---
+++ src/main/header.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react'; 
+ 
+ 
+export default class Header extends Component{
+	constructor(props) {
+		super(props); 
+		this.state = {
+
+			
+		}; 
+	 }
+	 
+ 
+	 
+	
+
+	render() { 
+		return (
+            <div className="header-container"> Header </div>
+
+		)
+	}
+}
+
